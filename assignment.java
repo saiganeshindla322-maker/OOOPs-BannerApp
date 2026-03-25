dbc8a052f99413796c5064c7aa163b4616696f8f
Sai Ganesh <saiganeshindla322@gmail.com>
2026-03-26 03:04:57 +0530

    Create assignment.java

---
+++ assignment.java
@@ -0,0 +1,10 @@
+class assignment {
+    public static void main(String[ ] args){
+        String name="Harry";
+        int birthyear=2000;
+        int currentyear=2024;
+        int current_age=(currentyear-birthyear);
+        System.out.println(name+"'s age in "+currentyear+" is "+current_age);
+
+    }
+}
